a93d9f74b0196f50ae1eeb58c7a5d614318efaf3
JatinGupta08 <136158885+JatinGupta08@users.noreply.github.com>
2023-09-15 12:11:17 +0530

    First Commit
    First Commit

---
+++ thetechharvest/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace thetechharvest.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+
+            return View();
+        }
+
+
+        public ActionResult OurCompany()
+        {
+            return View();
+        }
+
+        public ActionResult Partner()
+        {
+            return View();
+        }
+
+        public ActionResult Career()
+        {
+            return View();
+        }
+
+      
+        public ActionResult PrivacyPolicy()
+        {
+            return View();
+        }
+        public ActionResult CareerDetails(string id)
+        {
+            ViewBag.JID = id;
+            return View();
+        }
+
+    }
+}
